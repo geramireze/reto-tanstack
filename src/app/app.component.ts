2bee4198592fa72bf1254a2bcf8c460366af4d85
Gustavo Ramirez <gustavo.ramirez@avaldigitallabs.com>
2024-08-02 10:10:19 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { injectMutation, injectQuery, injectQueryClient, QueryObserver } from '@tanstack/angular-query-experimental';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,81 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'reto-tanstack';
+
+  todoService = inject(TodoService);
+  queryClient = injectQueryClient();
+
+  observer$ = new QueryObserver(this.queryClient, { queryKey: ['posts'] });
+
+  ngOnInit(): void {
+    this.data();
+  }
+
+  data() {
+    this.observer$.subscribe((result: any) => {
+      console.log('result', result);
+    });
+  }
+
+  query = injectQuery(() => ({
+    queryKey: ['todos'],
+    queryFn: () => this.todoService.getTodos(),
+  }));
+
+  queryPost = injectQuery(() => ({
+    queryKey: ['posts'],
+    queryFn: () => this.todoService.getPosts(),
+  }));
+
+
+
+  mutation = injectMutation((client) => ({
+    mutationFn: (todo: Todo) => this.todoService.addTodo(todo),
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ['todos'] });
+    },
+  }));
+
+  onAddTodo() {
+    this.mutation.mutate({
+      id: Date.now().toString(),
+      title: 'Do Laundry',
+    });
+  }
 }
+
+@Injectable({ providedIn: 'root' })
+export class TodoService {
+  private http = inject(HttpClient);
+
+  getTodos(): Promise<Todo[]> {
+    return lastValueFrom(
+      this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+    );
+  }
+
+  getPosts(): Promise<Post[]> {
+    return lastValueFrom(
+      this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+    );
+  }
+
+  addTodo(todo: Todo): Promise<Todo> {
+    return lastValueFrom(
+      this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+    );
+  }
+}
+
+interface Todo {
+  id: string;
+  title: string;
+}
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string
+}
+
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideAngularQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideAngularQuery(new QueryClient()),
+  ],
 };
